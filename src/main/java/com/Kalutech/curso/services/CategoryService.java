c079c86468ddc2704445629087be21fb07ed48a8
MS-marcone <sr.marcone@icloud.com>
2024-05-06 11:02:45 -0300

    incluido demais funcoes de produtos e categorias

--- src/main/java/com/Kalutech/curso/services/CategoryService.java
+++ src/main/java/com/Kalutech/curso/services/CategoryService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.Kalutech.curso.entities.Category;
 import com.Kalutech.curso.repositories.CategoryRepository;
+import com.Kalutech.curso.services.exceptions.DataBaseException;
+import com.Kalutech.curso.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -24,8 +30,35 @@ public class CategoryService {
 		return obj.get();
 	}
 	
-	//public User create() {
-	//	Optional<User> obj = repository.save(null);
-	//}
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+
+	public Category update(Long id, Category obj) {
+		try {
+		Category entity = repository.getReferenceById(id);
+		updatedata(entity, obj);
+		return repository.save(entity);
+		} catch( EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
+
+	private void updatedata(Category entity, Category obj) {
+		entity.setName(obj.getName());
+		
+
+	}
 
 }
